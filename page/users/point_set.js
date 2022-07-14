e4536b75bfdbe1be6069e0c75412c9018f849687
xiaoba <liuchang0318@autohome.com.cn>
2022-07-14 17:57:09 +0800

    update

---
+++ page/users/point_set.js
@@ -0,0 +1,28 @@
+var page = new Vue({
+    el: '#master_point_set_div',
+    data: {
+        pro_info: {
+            count: "0.00",
+            userId: 0,
+        },
+    },
+    methods: {
+        save_pro_info: function (proStatus) {
+            var roleUuid = $(":checked").val();
+            var _this = this;
+            console.log('lcc', _this.$route.query.uuid)
+            // HttpUtils.requestPost("/api/user/integralSetting", JSON.stringify(_this.pro_info), function (dataResult) {
+
+            //     if (dataResult.status == 1000) {
+            //         alert("操作成功!");
+            //         _this.find_pro_info(dataResult.data);
+            //     } else {
+            //         HttpUtils.showMessage("");
+            //     }
+            // });
+        },
+    },
+    mounted: function () {
+        this.save_pro_info();
+    }
+});
