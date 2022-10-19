03865dbc032ca438b32e6d8ebdcf1c91d53b77ff
xiaoba <liuchang0318@autohome.com.cn>
2022-10-19 20:38:39 +0800

    update

--- page/signManage/signmanagelist.js
+++ page/signManage/signmanagelist.js
@@ -5,19 +5,19 @@ var page = new Vue({
     },
     methods: {
         initData:function(){
-            var _this = this;
-            const href = window.location.href
-            if (href.indexOf('.dev.') >= 0) {
-                _this.userHost = 'https://gsk.test.100url.cn/' // 本地调试走代理
-            } else if (href.indexOf('.test.') >= 0) {
-                _this.userHost = 'https://gsk.test.100url.cn/'
-            } else if (href.indexOf('localhost') >= 0 ){
-                _this.userHost = ''
-            } else if (href.indexOf('skyyh') >= 0 ){
-                _this.userHost = 'https://skyyh.healthy-bone.com.cn/' // gsk生产
-            } else {
-                _this.userHost = 'https://healthy-bone.com.cn/'    // gsk测试
-            }
+            // var _this = this;
+            // const href = window.location.href
+            // if (href.indexOf('.dev.') >= 0) {
+            //     _this.userHost = 'https://gsk.test.100url.cn/' // 本地调试走代理
+            // } else if (href.indexOf('.test.') >= 0) {
+            //     _this.userHost = 'https://gsk.test.100url.cn/'
+            // } else if (href.indexOf('localhost') >= 0 ){
+            //     _this.userHost = ''
+            // } else if (href.indexOf('skyyh') >= 0 ){
+            //     _this.userHost = 'https://skyyh.healthy-bone.com.cn/' // gsk生产
+            // } else {
+            //     _this.userHost = 'https://healthy-bone.com.cn/'    // gsk测试
+            // }
             
         }
     },
