0c9e22d0c0a046e52b0f7d4e96171b39edd3fdf7
xiaoba <liuchang0318@autohome.com.cn>
2022-12-09 10:02:10 +0800

    update

--- page/users/verify_list.js
+++ page/users/verify_list.js
@@ -2,9 +2,16 @@ var page = new Vue({
     el: '#master_user_verify_list_div',
     data: {
         verify_datas: [],
-        pageIndex:0
+        pageIndex:0,
+       
+        currentPage: 1,
+        total: 1,
     },
     methods: {
+        handleCurrentChange(val) {
+            this.currentPage = val;
+            this.search_datas(val);
+        },
         save_verify_status: function (index,isSuccess) {
 
             var _this = this;
@@ -17,7 +24,7 @@ var page = new Vue({
                 };
                 HttpUtils.requestPost("/api/user/saveVerifyStatus", JSON.stringify(jsonData), function (dataResult) {
                     if (dataResult.status == 1000) {
-                        _this.search_datas(true)
+                        _this.search_datas(1)
                         $.toast("操作成功!");
                     } else {
                         $.toast("操作失败!");
@@ -27,54 +34,57 @@ var page = new Vue({
                 //取消操作
             });
         },
-        search_datas: function (isSearch) {
+        search_datas: function (val) {
             var _this = this;
 
-            if(isSearch){
-                _this.pageIndex=0;
-                _this.user_datas=[];
-            }
+            // if(isSearch){
+            //     _this.pageIndex=0;
+            //     _this.user_datas=[];
+            // }
 
             var jsonData = {
                 key: $("#mobile").val(),
                 content: $("#content").val(),
-                pageIndex:_this.pageIndex
+                pageIndex:val,
+                pageSize: 10,
             };
 
             HttpUtils.requestPost("/api/user/searchVerifyInfos", JSON.stringify(jsonData), function (dataResult) {
                 if (dataResult.status == 1000) {
 
-                    if(isSearch){
-                        _this.verify_datas = dataResult.data;
-                    }else{
-                        if(dataResult.data!=null){
-                            for(var i=0;i<dataResult.data.length;i++){
-                                _this.verify_datas.push(dataResult.data[i]);
-                            }
-                        }
-                    }
+                    // if(isSearch){
+                    //     _this.verify_datas = dataResult.data;
+                    // }else{
+                    //     if(dataResult.data!=null){
+                    //         for(var i=0;i<dataResult.data.length;i++){
+                    //             _this.verify_datas.push(dataResult.data[i]);
+                    //         }
+                    //     }
+                    // }
                    
-                    _this.pageIndex=_this.pageIndex+1;
+                    // _this.pageIndex=_this.pageIndex+1;
+                    _this.verify_datass = dataResult.data.list;
+                    _this.total = dataResult.data.total;
                 }
             });
         }
     },
     mounted: function () {
         var _this=this;
-        this.search_datas(true);
+        this.search_datas(1);
 
-        $(window).scroll(function () {
-			var scrollTop = $(this).scrollTop();
-			var scrollHeight = $(document).height();
-            var windowHeight = $(this).height();
-            var href=location.href;
-			if (scrollTop + windowHeight >= scrollHeight) {
+        // $(window).scroll(function () {
+		// 	var scrollTop = $(this).scrollTop();
+		// 	var scrollHeight = $(document).height();
+        //     var windowHeight = $(this).height();
+        //     var href=location.href;
+		// 	if (scrollTop + windowHeight >= scrollHeight) {
                 
-                if(href.indexOf("user_verify_list")>0){
-                    _this.search_datas(false);
-                } //alert("已经到最底部了！");　
+        //         if(href.indexOf("user_verify_list")>0){
+        //             _this.search_datas(false);
+        //         } //alert("已经到最底部了！");　
 				
-			}
-		});
+		// 	}
+		// });
     }
 });
